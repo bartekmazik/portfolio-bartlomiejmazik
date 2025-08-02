3ae05afe3c3e95cf36424ae2155c023c1348e752
bartek <log.mazik.bartek@gmail.com>
2025-08-02 19:19:40 +0200

    border footer fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="w-screen h-[10vh] flex font-semibold flex-row items-end justify-center pb-6">
+    <footer className="w-screen h-[10vh] flex font-semibold flex-row items-end justify-center pb-6 border-t-4 border-black">
       Bartłomiej Mazik 2025
     </footer>
   );
